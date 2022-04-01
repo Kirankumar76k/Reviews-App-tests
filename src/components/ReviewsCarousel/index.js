4744ee03955eb233a4466850bec894bdd7e0c4de
kiran kumar <kirankumar76k>
2022-04-01 11:55:20 +0000

    Reviews App

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,77 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {activeReviewIndex: 0}
+
+  onClickLeftArrow = () => {
+    const {activeReviewIndex} = this.state
+    if (activeReviewIndex > 0) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex - 1,
+      }))
+    }
+  }
+
+  onClickRightArrow = () => {
+    const {reviewsList} = this.props
+    const {activeReviewIndex} = this.state
+    if (activeReviewIndex < reviewsList.length - 1) {
+      this.setState(prevState => ({
+        activeReviewIndex: prevState.activeReviewIndex + 1,
+      }))
+    }
+  }
+
+  getReviewList = review => {
+    const {imgUrl, username, companyName, description} = review
+    return (
+      <div className="review-container">
+        <img src={imgUrl} alt={username} className="img-cls" />
+        <p className="name">{username}</p>
+        <p className="company">{companyName}</p>
+        <p className="description">{description}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {reviewsList} = this.props
+    const {activeReviewIndex} = this.state
+    const activeReviewList = reviewsList[activeReviewIndex]
+
+    return (
+      <div className="bg-container">
+        <h1 className="heading">Reviews</h1>
+
+        <div className="carousel-container">
+          <button
+            className="button"
+            type="button"
+            onClick={this.onClickLeftArrow}
+            testid="leftArrow"
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+              alt="left arrow"
+            />
+          </button>
+          {this.getReviewList(activeReviewList)}
+          <button
+            className="button"
+            type="button"
+            onClick={this.onClickRightArrow}
+            testid="rightArrow"
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+              alt="right arrow"
+            />
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+export default ReviewsCarousel
